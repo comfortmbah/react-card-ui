1b658ebf0a7aa225903f3021c447f31222afd0ec
sylvester <sylvestermbah90@gmail.com>
2026-05-26 16:18:08 -0700

    Import resuable Card component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Card } from "./Components/Card"
+
+
+function App() {
+  
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+    </div>
+  )
+}
+
+export default App
