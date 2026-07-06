f4f2deeba24a8cec7108f74296a68b97bd6fe712
sylvester <sylvestermbah90@gmail.com>
2026-07-06 04:24:38 -0700

    Add image to card.jsx and update tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
       <Card 
-        image={''}
+        image={'/profile1.jpg'}
         title={'Nature View'}
         description={'A clean reusable React card component styled with TailwindCSS'}
         buttonText={'Explore'}
